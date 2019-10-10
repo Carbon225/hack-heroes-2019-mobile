5e425ab0d08e5463fa450e01a7ea86c08e365178
Carbon <carbon.szlachta@gmail.com>
2019-10-10 22:29:34 +0200

    Firebase messaging

--- lib/ui/network_stats_card.dart
+++ lib/ui/network_stats_card.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,6 +8,41 @@ class NetworkStatsCard extends StatefulWidget {
 }
 
 class NetworkStatsCardState extends State<NetworkStatsCard> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    print('STARTING FIREBASE PLSSSS WORK');
+    _firebaseMessaging.configure(
+//      onBackgroundMessage: (msg) async => print('BG msg'),
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
